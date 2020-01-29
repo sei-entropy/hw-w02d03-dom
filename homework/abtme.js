e9184757f656e325e26730f1554c351a62d4692d
aLi <aaljamid@emich.edu>
2020-01-29 08:31:55 -0500

    y hw v3

--- homework/abtme.js
+++ homework/abtme.js
@@ -25,4 +25,20 @@ const liClass = document.querySelectorAll("li");
 
 for (i = 0; i < liClass.length; i++) {
   liClass[i].setAttribute("class", "listitem");
+  liClass[i].setAttribute("style", "color:rebeccapurple;");
 }
+
+const newImg = document.createElement("img");
+
+const newImgDiv = document.querySelector(".profilePicture");
+
+newImgDiv.appendChild(newImg);
+
+const myimg = document.querySelector("img");
+
+myimg.setAttribute(
+  "src",
+  "https://i.pinimg.com/originals/2c/af/b6/2cafb6f0a0e5a5b68e97d2723c0f8ea7.png"
+);
+
+myimg.setAttribute("style", "width:100px;");

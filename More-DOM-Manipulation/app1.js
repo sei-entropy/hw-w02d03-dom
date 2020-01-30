56fd343d72a0b529caf7459173535ffb7831b824
maimonah95 <mim95s@hotmail.com>
2020-01-30 19:35:39 +0300

    update

--- More-DOM-Manipulation/app1.js
+++ More-DOM-Manipulation/app1.js
@@ -37,3 +37,72 @@ let addImg = document.querySelector('.profilePicture');
 addImg.appendChild(myImg);
 
 //DONE
+///book.js
+
+var books = [{
+        title: "The Design of Everyday Things",
+        author: "Don Norman",
+        alreadyRead: false
+    },
+    {
+        title: "The Most Human Human",
+        author: "Brian Christian",
+        alreadyRead: true
+    },
+    {
+        title: "In Search of Lost Time",
+        author: "Marcel Proust",
+        alreadyRead: true
+    },
+    {
+        title: "Ulysses",
+        author: "James Joyce",
+        alreadyRead: true
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        alreadyRead: true
+    }
+];
+//1
+const newH = document.createElement('h1');
+newH.innerText = 'My Book List';
+const targetDiv = document.querySelector('.favoriteBooks');
+targetDiv.appendChild(newH);
+
+//2
+
+//let myBook = document.createElement('p');
+// for (let i = 0; i < books.length; i++) {
+//     let myBook = document.createElement('p');
+//        myBook.innerText = (books[i].title + 'by' + books[i].author);
+
+//      let targetbook = document.querySelector('body');
+//     targetbook.appendChild(myBook);
+   
+// }
+//Done
+//bonus
+//1 && 3
+let underList = document.createElement('ul');
+for (let i = 0; i < books.length; i++) {
+    let bok = document.createElement('li');
+          bok.innerText = (books[i].title + ' by ' + books[i].author);
+           if (books[i].alreadyRead) {
+               bok.style.color = 'red';
+           };
+          let targetbook = document.querySelector('body');
+          targetbook.appendChild(bok);
+       
+          
+    //bok.appendChild(books[i].title);
+    //2
+    // let imag = document.createElement('img');
+    // imag.src = books[i].imag;
+    // imag.appendChild(books[i].imag);
+    // //3
+    // if (book[i].alreadyRead) {
+    //     bok.style.color = 'red';
+    // }
+}

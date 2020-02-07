b1ea686fe137c048fc04c1ee9a644bdf0e1304ca
RanenDev <github/RanenDev>
2020-02-07 12:55:07 +0300

    solution

--- randomImages/app.js
+++ randomImages/app.js
@@ -1,6 +1,29 @@
-var imagesArray = ["https://source.unsplash.com/1600x900", "https://source.unsplash.com/1600x900", "https://source.unsplash.com/1600x900", "https://source.unsplash.com/1600x900", "https://source.unsplash.com/1600x900"];
+//Botton Random image
+const randomizeIm = document.querySelector('#randomize');
 
-function newRandomImage() {
-    let num = Math.floor(Math.random() * 5);
-    document.canvas.src = imagesArray[num];
-} 
+//callback function
+const createImage =function(){
+
+//Bonus 1
+//The image width and height
+   const width = document.querySelector('.width');
+   const widthContent = width.value;
+
+   const height = document.querySelector('.height');
+   const heightContent = height.value;
+
+//Create new image element and give 'src' property & set its width and height
+   const url="https://source.unsplash.com/"+widthContent+"x"+heightContent;
+   const image = document.createElement('img');
+   image.setAttribute('src',url);
+
+//Appending the generated img to the page
+//inside the div with the image class
+    const imageDiv = document.querySelector('.image');
+    imageDiv.appendChild(image);
+    width.value = '';
+    height.value = '';
+}
+
+//Adding event listner for the button click
+randomizeIm.addEventListener('click',createImage);
